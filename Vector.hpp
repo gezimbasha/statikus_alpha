7bb132484ee0c311a06fbcde943373e9c2001d2a
Gëzim Basha <gezimbasha@outlook.com>
2013-05-14 00:36:37 +0200

    Highly functional Vector class implemented

--- Vector.hpp
+++ Vector.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -8,18 +9,48 @@ class Vector
 {
 	T x[n];
 public:
+	//Constructors
 	Vector(void);
 	Vector(T*, unsigned int);
+	Vector(T);
 	~Vector(void);
 
 	// Accessors
-	T coeff(unsigned int);
+	T coeff(unsigned int) const;
 	void coeff(T, unsigned int);
 
-	// Operators
-	template<class T, unsigned int n> friend ostream& operator << (ostream&, Vector<T,n>&);
+	// Friend Operators
+	template<class T, unsigned int n> friend ostream& operator << (ostream&, const Vector<T,n>&);
 
-	// Class technical information
+	// Accessor Operators
+	T operator[](unsigned int);
+	T operator()(unsigned int);
+	
+	// Compound Assignment Operators
+	Vector& operator=(const Vector &);
+	Vector& operator+=(const Vector &);
+	Vector& operator-=(const Vector &);
+	Vector& operator*=(const T);
+	Vector& operator/=(const T);
+	
+	// Binary Arithmetic Operators
+	const Vector operator + (const Vector &) const;
+	const Vector operator - (const Vector &) const;
+	const Vector operator * (const T ) const;
+	const Vector operator / (const T ) const;
+
+	// Boolean Operators
+	bool operator==(const Vector &) const;
+	bool operator!=(const Vector &) const;
+
+	// Vector Operations
+	Vector operator - (void) const;
+	Vector normalize(void) const;
+	T operator*(const Vector &) const;
+		
+	T magnitude() const;
+	T dot(const Vector&) const;
+	// Technical Information
 	unsigned int size();
 };
 
@@ -45,6 +76,14 @@ Vector<T,n>::Vector(T* _data, unsigned int _n)
 		x[i] = _data[i];
 }
 
+//Uniform seed of t
+template<class T, unsigned int n>
+Vector<T,n>::Vector(T t)
+{
+	for(unsigned int i=0; i<n; i++)
+		x[i] = t;
+}
+
 template<class T, unsigned int n>
 Vector<T,n>::~Vector(void)
 {
@@ -55,7 +94,7 @@ Vector<T,n>::~Vector(void)
 //////////////////////////////////////////////////////////
 
 template<class T, unsigned int n>
-T Vector<T, n>::coeff(unsigned int i)
+T Vector<T, n>::coeff(unsigned int i) const
 {
 	return x[i];
 }
@@ -73,12 +112,175 @@ void Vector<T, n>::coeff(T t, unsigned int i)
 // Operators
 //////////////////////////////////////////////////////////
 template<class T, unsigned int n>
-ostream& operator << (ostream& os, Vector<T,n> &A)
+ostream& operator << (ostream& os, const Vector<T,n> &A)
 {
 	for(int i=0; i<n; i++)
 		os << A.coeff(i) << endl;
 
 	return os;
+}
+
+template<class T, unsigned int n>
+T Vector<T,n>::operator[](unsigned int i)
+{
+	return (x[i]);
+}
+
+template<class T, unsigned int n>
+T Vector<T,n>::operator()(unsigned int i)
+{
+	return (x[i]);
+}
+
+template<class T, unsigned int n>
+Vector<T,n>& Vector<T,n>::operator=(const Vector<T,n> &rhs)
+{
+	if(this != &rhs)
+	{
+		for(unsigned int i=0; i<n; i++){
+			this->coeff(rhs.coeff(i),i);
+		}
+	}
+		
+	return *this;
+}
+
+template<class T, unsigned int n>
+Vector<T,n>& Vector<T,n>::operator+=(const Vector<T,n> &rhs)
+{
+	for(unsigned int i=0; i<n; i++)
+		this->coeff((this->coeff(i)+rhs.coeff(i)),i);
+	return *this;
+}
+
+template<class T, unsigned int n>
+Vector<T,n>& Vector<T,n>::operator-=(const Vector<T,n> &rhs)
+{
+	for(unsigned int i=0; i<n; i++)
+		this->coeff((this->coeff(i)-rhs.coeff(i)),i);
+	return *this;
+}
+
+template<class T, unsigned int n>
+Vector<T,n>& Vector<T,n>::operator*=(const T t)
+{
+	for(unsigned int i=0; i<n; i++)
+		this->coeff((this->coeff(i)*t),i);
+	return *this;
+}
+
+template<class T, unsigned int n>
+Vector<T,n>& Vector<T,n>::operator/=(const T t)
+{
+	// Division by zero not allowed, return identity;
+	if(t == 0) return *this;
+
+	for(unsigned int i=0; i<n; i++)
+		this->coeff((this->coeff(i)/t),i);
+	return *this;
+
+}
+
+template<class T, unsigned int n>
+const Vector<T,n> Vector<T,n>::operator+(const Vector<T,n> &rhs) const
+{
+	Vector<T,n> rs = *this;
+	rs += rhs;
+	return rs;
+}
+
+template<class T, unsigned int n>
+const Vector<T,n> Vector<T,n>::operator-(const Vector<T,n> &rhs) const
+{
+	Vector<T,n> rs = *this;
+	rs -= rhs;
+	return rs;
+}
+
+template<class T, unsigned int n>
+const Vector<T,n> Vector<T,n>::operator*(const T rhs) const
+{
+	Vector<T,n> rs = *this;
+	rs *= rhs;
+	return rs;
+}
+
+template<class T, unsigned int n>
+const Vector<T,n> Vector<T,n>::operator/(const T rhs) const
+{
+	Vector<T,n> rs = *this;
+	rs /= rhs;
+	return rs;
+}
+
+template<class T, unsigned int n>
+bool Vector<T,n>::operator==(const Vector<T,n> &rhs) const
+{
+	int j=0;
+	for(unsigned int i=0; i<n; i++){
+		//Increase j by one for each true value here
+		if(this->coeff(i) == rhs.coeff(i))
+			j++;
+	}
+	
+	//If j==n it means that every checked coefficient returned true
+	if(j==n)
+		return true;
+	else
+		return false;
+}
+
+template<class T, unsigned int n>
+bool Vector<T,n>::operator!=(const Vector<T,n> &rhs) const
+{
+	return !(*this == rhs);
+}
+
+template<class T, unsigned int n>
+Vector<T,n> Vector<T,n>::operator - (void) const 
+{
+	Vector<T,n> res = *this;
+	res*=-1;
+	return res;
+}
+
+//////////////////////////////////////////////////////////
+// Vector Operations
+//////////////////////////////////////////////////////////
+
+template<class T, unsigned int n>
+T Vector<T,n>::magnitude() const 
+{
+	T res = 0;
+	for(unsigned int i=0; i<n; i++)
+		res += pow(x[i],2);
+
+	return sqrt(res);
+}
+
+template<class T, unsigned int n>
+Vector<T,n> Vector<T,n>::normalize(void) const 
+{
+	Vector<T,n> lhs = *this;
+	T mag = lhs.magnitude();
+	lhs /= mag;
+	return lhs;
+}
+
+template<class T, unsigned int n>
+T Vector<T,n>::dot(const Vector &rhs) const 
+{
+	Vector<T,n> lhs = *this;
+	T res = 0;
+	for(unsigned int i=0; i<n; i++)
+		res += (lhs.coeff(i)*rhs.coeff(i));
+	return res;
+}
+
+template<class T, unsigned int n>
+T Vector<T,n>::operator*(const Vector &rhs) const 
+{
+	return Vector<T,n>::dot(rhs);
 }
 
 //////////////////////////////////////////////////////////
