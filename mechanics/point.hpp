00856fff830cf9be2a312b5628f5dcca8b418c6e
gezimbasha <gezimbasha@outlook.com>
2013-05-16 11:16:17 +0200

    Torque and forces added to Points

--- mechanics/point.hpp
+++ mechanics/point.hpp
@@ -1,11 +1,21 @@
 #pragma once
 #include "../math/vector.hpp"
+#include <vector>
+using namespace std;
+
+// Standard axis enumeration
+enum axis {
+	x,
+	y,
+	z
+};
 
 template<class T, unsigned int n>
 class Point
 {
 private:
 	Vector<T,n> m_vPosition;
+	Vector<T,n> m_vForces;
 public:
 	Point();
 	Point(Vector<T,n> pos);
@@ -14,11 +24,20 @@ public:
 
 	// Position Accessors
 	Vector<T,n> position();
+	T component(axis);
 	void setPosition(Vector<T,n>);
+	
+	// Forces
+	Vector<T,n> force();
+	void addForce(Vector<T,n>);
+	void addForces(Vector<T,n>,...);
 
 	// Distance
 	T distance(Point);
-	T distance(Point,unsigned int);
+	T distance(Point,axis);
+
+	// Mechanics
+	Vector<T,n> torque(Point);
 };
 
 /////////////////////////////////////////////
@@ -69,6 +88,12 @@ void Point<T,n>::setPosition(Vector<T,n> v)
 }
 
 template<class T, unsigned int n>
+T Point<T,n>::component(axis i)
+{
+	return m_vPosition.coeff(i);
+}
+
+template<class T, unsigned int n>
 T Point<T,n>::distance(Point<T,n> pos)
 {
 	Vector<T,n> dist = this->position()-pos.position();
@@ -76,10 +101,51 @@ T Point<T,n>::distance(Point<T,n> pos)
 }
 
 template<class T, unsigned int n>
-T Point<T,n>::distance(Point<T,n> pos, unsigned int i)
+T Point<T,n>::distance(Point<T,n> pos, axis i)
 {
 	Vector<T,n> dist = this->position()-pos.position();
 	return dist.coeff(i);
+}
+
+/////////////////////////////////////////////
+// Force Accessors
+////////////////////////////////////////////
+
+template<class T, unsigned int n>
+Vector<T,n> Point<T,n>::force()
+{
+	return m_vForces;
+}
+
+template<class T, unsigned int n>
+void Point<T,n>::addForce(Vector<T,n> F)
+{
+	m_vForces += F;
+}
+
+
+template<class T, unsigned int n>
+void Point<T,n>::addForces(Vector<T,n> V, ...)
+{
+	typedef Vector<T,n> F;
+	F F0 = V;
+	m_vForces += F0;
+	va_list args;
+	va_start(args, V);	
+	m_vForces += va_arg(args,F);
+	va_end(args);
+}
+
+/////////////////////////////////////////////
+// Mechanics
+////////////////////////////////////////////
+
+template<class T, unsigned int n>
+Vector<T,n> Point<T,n>::torque(Point<T,n> P)
+{
+	Point<T,n> tmp = *this;
+	Vector<T,n> df =  P.position()-tmp.position();
+	return df.cross(P.force());
 }
 
 /////////////////////////////////////////////
